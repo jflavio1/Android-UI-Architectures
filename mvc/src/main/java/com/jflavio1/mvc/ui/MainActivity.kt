8e2ee6998b0c9787079284dfb4d1b26261fea9ed
JoseFlavio <jflavio90@gmail.com>
2018-05-19 14:08:21 -0500

    mvc packages

--- mvc/src/main/java/com/jflavio1/mvc/ui/MainActivity.kt
+++ mvc/src/main/java/com/jflavio1/mvc/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.jflavio1.mvc
+package com.jflavio1.mvc.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.jflavio1.mvc.R
 
 class MainActivity : AppCompatActivity() {
 
