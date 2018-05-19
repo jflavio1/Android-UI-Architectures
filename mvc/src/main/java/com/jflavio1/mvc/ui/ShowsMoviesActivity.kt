66713cc8ebfa0d3f7888c90280c1fe8a99200b33
JoseFlavio <jflavio90@gmail.com>
2018-05-19 14:10:30 -0500

    renamed mainactivity to ShowMoviesActivity

--- mvc/src/main/java/com/jflavio1/mvc/ui/ShowsMoviesActivity.kt
+++ mvc/src/main/java/com/jflavio1/mvc/ui/ShowsMoviesActivity.kt
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.jflavio1.mvc.R
 
-class MainActivity : AppCompatActivity() {
+class ShowsMoviesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
